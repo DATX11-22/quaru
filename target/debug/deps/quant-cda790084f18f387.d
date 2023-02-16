de6bf539cacedfae832baf067722e7e6e9a935e5
Simon Johnsson <simzter@outlook.com>
2023-02-16 15:39:10 +0100

    Fix target directory testing dependencies

---
+++ target/debug/deps/quant-cda790084f18f387.d
@@ -0,0 +1,5 @@
+/home/simjohn/chalmers/bachelor_thesis/Quaru/target/debug/deps/quant-cda790084f18f387.rmeta: src/lib.rs
+
+/home/simjohn/chalmers/bachelor_thesis/Quaru/target/debug/deps/quant-cda790084f18f387.d: src/lib.rs
+
+src/lib.rs:

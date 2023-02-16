734f55604c81071a7d8d939785d409ab53783deb
Simon Johnsson <simzter@outlook.com>
2023-02-16 15:41:17 +0100

    Run clippy fix locally

---
+++ target/debug/deps/quant-84f4cdf66682c82a.d
@@ -0,0 +1,8 @@
+/home/simjohn/chalmers/bachelor_thesis/Quaru/target/debug/deps/quant-84f4cdf66682c82a.rmeta: src/lib.rs Cargo.toml
+
+/home/simjohn/chalmers/bachelor_thesis/Quaru/target/debug/deps/quant-84f4cdf66682c82a.d: src/lib.rs Cargo.toml
+
+src/lib.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=-W__CLIPPY_HACKERY__clippy::unwrap_used__CLIPPY_HACKERY__--no-deps__CLIPPY_HACKERY__

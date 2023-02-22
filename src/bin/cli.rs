72c079214897dedd6a4a9758508e586225e07518
Simon Johnsson <simzter@outlook.com>
2023-02-22 10:31:25 +0100

    Add initial prompt choices

--- src/bin/cli.rs
+++ src/bin/cli.rs
@@ -1,4 +1,52 @@
+use std::fmt::Display;
+
+use inquire::{error::InquireError, Select};
+
+enum Choice {
+    Show,
+    Apply
+}
+
+impl Choice {
+    fn choices() -> Vec<Choice> {
+        vec![Choice::Show, Choice::Apply]
+    }
+}
+
+impl Display for Choice {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Choice::Apply => write!(f, "Apply"),
+            Choice::Show => write!(f, "Show"),
+        }
+    }
+}
+
+enum UnaryOperation {
+    Identity,
+    Hadamard,
+    Phase,
+    NOT,
+    PauliX,
+    PauliY,
+    PauliZ,
+}
+
+enum BinaryOperation {
+    CNOT,
+    Swap,
+}
+
+/// Prompts the user for an initial choice.
+/// 
+/// Choices include:
+/// - Applying an operation 
+/// - Showing the register state
+fn initial_prompt() -> Result<Choice, InquireError> {
+    let options = Choice::choices(); 
+    Select::new("Select an option: ", options).prompt()
+}
 
 fn main() {
-    unimplemented!();
+    let initial = initial_prompt();
 }

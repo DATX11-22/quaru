9d49df3d7e102df28888a0cede17e10a891d4695
Elis Kleen <eliskleen00@gmail.com>
2023-03-01 14:47:43 +0100

    Superdense coding and quantum teleportation

--- tests/register_operation_tests.rs
+++ tests/register_operation_tests.rs
@@ -92,7 +92,7 @@ proptest!(
         if i == j {
             j = (j + 1) % 6;
         }
-        let hadamard = operation::hadamard(j);
+        let hadamard = operation::hadamard(i);
         let cnot = operation::cnot(i, j);
 
         // maximally entangle qubit i and j
@@ -108,14 +108,14 @@ proptest!(
     // #[ignore = "Indexing issue in register, is weird"]
     fn first_bell_state_measure_equal(i in 0..5 as usize) {
         let mut reg = Register::new(&[false; 6]);
-        let hadamard = operation::hadamard(i+1);
+        let hadamard = operation::hadamard(i);
         let cnot = operation::cnot(i, i + 1);
 
         // maximally entangle qubit i and i + 1
         reg.apply(&hadamard);
         reg.apply(&cnot);
 
-        reg.print_probabilities();
+        // reg.print_probabilities();
         // this should measure index i and i+1
         assert_eq!(reg.measure(i), reg.measure(i+1));
     }
@@ -156,63 +156,133 @@ proptest!(
 
 
     #[test]
-    // #[ignore = "Apply does not figure out how to swap bits"]
-    fn quantum_teleportation(q0 in StateVector::arbitrary_with(())) {
-        let mut reg = Register::new(&[false;3]);
+    fn quantum_teleportation(q0 in Qubit::arbitrary_with(())) {
 
-
-
+        let expected = q0.0.clone();
+        let mut reg = Register::new(&[false, false, true]);
         let base_state = array![[Complex::new(1.0, 0.0)]]; 
-        let expected = q0.clone();
-
-        reg.state = [q0.0, to_qbit_vector(&false) , to_qbit_vector(&false)]
+        let new_state = [expected.clone(), to_qbit_vector(&false) , to_qbit_vector(&false)]
                     .iter()
                     .fold(base_state, |a, b| linalg::kron(&b, &a));
+        reg.state = new_state.clone();
+        
+        reg.apply(&operation::hadamard(2));
         reg.apply(&operation::cnot(2, 1));
         reg.apply(&operation::cnot(0, 1));
         reg.apply(&operation::hadamard(0));
 
-        let c_0 = reg.measure(0);
         let c_1 = reg.measure(1);
         if c_1 {
             reg.apply(&operation::not(2));
         } 
+        let c_0 = reg.measure(0);
         if c_0 {
             reg.apply(&operation::pauli_z(2));
         }
-        let input = expected.clone(); //hur får jag tag i state vectorn för q2??
 
-        println!("Expected: {:?}", expected.0);
+        let input = get_state_of_qubit(reg.state.clone(), 2);
+        let is_equal = equal_qubits(input.clone(), expected.clone());
+        if !is_equal {
+            reg.print_probabilities();
+            println!("\n\n\nExpected: {:}", expected);
+            println!("Got: {:}\n\n\n", input);
+        }
+        reg.print_probabilities();
+        println!("input: {:}", input);
+        assert!(is_equal);
 
-        // reg.print_probabilities();
-        assert_eq!(expected, input);
+    }
 
+
+    fn superdense_coding(
+        m0 in any::<bool>(),
+        m1 in any::<bool>()
+    ) {
+        let message = [m0, m1];
+        let mut register = Register::new(&[false; 2]);
+
+        // Entangle qubit 0 and 1
+        register.apply(&operation::hadamard(1));
+        register.apply(&operation::cnot(0, 1));
+
+        register.print_probabilities();
+        println!();
+
+        // Encode the message
+        if message == [false, false] {
+            register.apply(&operation::identity(1));
+        } else if message == [false, true] {
+            register.apply(&operation::not(1));
+        } else if message == [true, false] {
+            register.apply(&operation::pauli_z(1));
+        } else if message == [true, true] {
+            register.apply(&operation::pauli_z(1));
+            register.apply(&operation::not(1));
+        }
+
+        register.print_probabilities();
+        println!();
+
+        // Decode message
+        register.apply(&operation::cnot(0, 1));
+        register.apply(&operation::hadamard(1));
+
+        register.print_probabilities();
+        println!();
+
+        println!("Result: {}{}", register.measure(1) as i32, register.measure(0) as i32);
+        assert_eq!(message, [register.measure(1), register.measure(0)]);
     }
 
 );
 
+pub fn equal_qubits(a : Array2<Complex<f64>>, b : Array2<Complex<f64>>) -> bool {
+    let mut equal = true;
+    for (i, s) in a.iter().enumerate() {
+        // denna kan vara lite för hård, -a = a eftersom de har samma sannolikhet
+        if (s - b[(i, 0)]).norm() >= 1e-8 {
+            equal = false;
+        }
+    }
+    equal
+}
 pub fn to_qbit_vector(bit: &bool) -> Array2<Complex<f64>> {
     match bit {
         true => array![[Complex::new(0.0, 0.0)], [Complex::new(1.0, 0.0)]],
         false => array![[Complex::new(1.0, 0.0)], [Complex::new(0.0, 0.0)]],
     }
 }
-
+pub fn get_state_of_qubit(state : ArrayBase<OwnedRepr<Complex<f64>>, Dim<[usize; 2]>>, n : usize)  -> Array2<Complex<f64>> {
+    let mut beta : Complex<f64> = Complex::new(0.0, 0.0);
+    let mut alpha : Complex<f64> = Complex::new(0.0, 0.0);
+    for (i, s) in state.iter().enumerate() {
+        if (i >> n) & 1 == 1 {
+            beta += s;
+        }
+        else  {
+            alpha += s;
+        }
+    }
+    let prob = alpha.norm().powi(2) + beta.norm().powi(2);
+    alpha /= prob.sqrt();
+    beta /= prob.sqrt();
+    array![[alpha], [beta]]
+}
 
 #[derive(Clone, Debug, PartialEq)]
-struct StateVector(ArrayBase<OwnedRepr<Complex<f64>>, Dim<[usize; 2]>>);
+pub struct Qubit(Array2<Complex<f64>>);
 
-impl Arbitrary for StateVector {
+impl Arbitrary for Qubit {
     type Parameters = ();
-    type Strategy = Select<StateVector>;
+    type Strategy = Select<Qubit>;
     fn arbitrary_with(args: Self::Parameters) -> Self::Strategy {
         let frac = consts::FRAC_1_SQRT_2;
         select(vec!(
-           StateVector(array![[Complex::new(1.0, 0.0), Complex::new(0.0, 0.0)]]),
-           StateVector(array![[Complex::new(0.0, 0.0), Complex::new(1.0, 0.0)]]),
-           StateVector(array![[Complex::new(frac, 0.0), Complex::new(frac, 0.0)]]),
-           StateVector(array![[Complex::new(frac, 0.0), Complex::new(-frac, 0.0)]]),
-           StateVector(array![[Complex::new(frac, 0.0), Complex::new(0.0, -frac)]]),
+           Qubit(array![[Complex::new(1.0, 0.0)], [Complex::new(0.0, 0.0)]]),
+           Qubit(array![[Complex::new(0.0, 0.0)], [Complex::new(1.0, 0.0)]]),
+           Qubit(array![[Complex::new(frac, 0.0)], [Complex::new(frac, 0.0)]]),
+           Qubit(array![[Complex::new(frac, 0.0)], [Complex::new(-frac, 0.0)]]),
+           Qubit(array![[Complex::new(frac, 0.0)], [Complex::new(0.0, -frac)]]),
         ))
        
     }

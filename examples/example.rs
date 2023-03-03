0794e725b067ffa28b5bbb48b4daf3f48580eadf
Elis Kleen <eliskleen00@gmail.com>
2023-03-03 16:58:06 +0100

    started with shores

--- examples/example.rs
+++ examples/example.rs
@@ -1,30 +1,50 @@
+use gcd::Gcd;
 use ndarray::array;
 use num::Complex;
-use quant::register::Register;
+use quant::{operation, register::Register};
 
+use ndarray::{Array2, ArrayBase, Data, DataMut};
 
 fn main() {
-    let mut register = Register::new(&[false, true, true]);
+    let reg = Register::new(&[false; 4]);
+    let N: u32 = 15;
+    let d1 = shores(N);
+    let d2 = N / d1;
+    println!("The factors of {} are {} and {}", N, d1, d2);
 
-    register.state = Complex::new(1.0 / f64::sqrt(4.0), 0.0) * array![
-        [Complex::new(0.0, 0.0)],
-        [Complex::new(0.0, 0.0)],
-        [Complex::new(1.0, 0.0)],
-        [Complex::new(1.0, 0.0)],
-        [Complex::new(1.0, 0.0)],
-        [Complex::new(1.0, 0.0)],
-        [Complex::new(0.0, 0.0)],
-        [Complex::new(0.0, 0.0)],
-    ];
+    // let qft = operation::qft(4);
+    // println!("{:}", qft.matrix)
+}
 
-    register.print_probabilities();
+fn shores(N: u32) -> u32 {
+    if N % 2 == 0 {
+        // N is even
+        return 2;
+    }
+    loop {
+        let mut a: u32 = rand::random();
+        a = a % N;
+        a = if a <= 1 { 2 } else { a };
+        // a = 7;
+        let K = gcd::euclid_u32(a, N);
+        if K != 1 && K != N {
+            return K;
+        }
 
-    println!("{}", register.measure(1));
-    let mut register = Register::new(&[true, false, false]);
+        println!("a = {}", a);
+        let r = find_r(N, a);
+        if r % 2 == 0 {
+            // r is even so we can find a factor
+            return gcd::euclid_u32(a.pow(r / 2) + 1, N)
+        }
+    }
+}
 
-    register.print_probabilities();
-
-    println!("{}", register.measure(0));
-
-    register.print_probabilities();
+//TODO: implement this function
+fn find_r(N: u32, a: u32) -> u32 {
+    if a == 7 && N == 15 {
+        return 4;
+    }
+    //calculate f(x) as a truth function
+    1
 }

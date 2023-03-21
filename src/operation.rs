cff0f8479f86c48b3cb071718bb65cf9085d8e31
Simon Johnsson <73998667+BAMF0@users.noreply.github.com>
2023-03-21 12:55:13 +0100

    Refactor operation.rs (#46)
    * Add constructor for operation
    
    * Add documentation and remove redundant attribute
    
    * The attribute arity  is redundant and should instead be represented by the number of targets. This is given by the arity function.
    
    * Move real_to_complex to math module and add doc
    
    * Resolve orphan parameter arity

--- src/operation.rs
+++ src/operation.rs
@@ -2,6 +2,8 @@ use ndarray::{array, Array2};
 use num::Complex;
 use std::{f64::consts, vec};
 
+use crate::math::real_to_complex;
+
 // Naming?
 pub trait OperationTrait {
     fn matrix(&self) -> Array2<Complex<f64>>;
@@ -9,11 +11,36 @@ pub trait OperationTrait {
     fn arity(&self) -> usize;
 }
 
+/// A quantum computer operation represented by a matrix and targets.
+///
+/// - `matrix` corresponds to the quantum operator
+/// - `targets` corresponds to the operator operands
+///
+/// # Note
+/// In order for an operation to be considered valid, the matrix shape must be square
+/// with length equal to the number of operands.
 #[derive(Clone, Debug)]
 pub struct Operation {
     matrix: Array2<Complex<f64>>,
     targets: Vec<usize>,
-    arity: usize,
+}
+
+impl Operation {
+    /// Constructs an operation with the given matrix and targets.
+    ///
+    /// Returns an operation if `matrix` is square with sides equal to number of `targets`.
+    ///
+    /// Otherwise, returns `None`.
+    pub fn new(matrix: Array2<Complex<f64>>, targets: Vec<usize>) -> Option<Operation> {
+        let shape = matrix.shape();
+        let len = targets.len();
+
+        if shape[0] != len || shape[1] != len {
+            return None;
+        }
+
+        Some(Operation { matrix, targets })
+    }
 }
 
 // TODO: Check if we can return references instead?
@@ -27,7 +54,7 @@ impl OperationTrait for Operation {
     }
 
     fn arity(&self) -> usize {
-        self.arity
+        self.targets().len()
     }
 }
 
@@ -35,7 +62,6 @@ pub fn identity(target: usize) -> Operation {
     Operation {
         matrix: real_to_complex(array![[1.0, 0.0], [0.0, 1.0]]),
         targets: vec![target],
-        arity: 1,
     }
 }
 
@@ -43,7 +69,6 @@ pub fn hadamard(target: usize) -> Operation {
     Operation {
         matrix: real_to_complex(consts::FRAC_1_SQRT_2 * array![[1.0, 1.0], [1.0, -1.0]]),
         targets: vec![target],
-        arity: 1,
     }
 }
 
@@ -56,7 +81,6 @@ pub fn cnot(control: usize, target: usize) -> Operation {
             [0.0, 0.0, 1.0, 0.0]
         ]),
         targets: vec![target, control],
-        arity: 2,
     }
 }
 
@@ -69,7 +93,6 @@ pub fn swap(target1: usize, target2: usize) -> Operation {
             [0.0, 0.0, 0.0, 1.0]
         ]),
         targets: vec![target1, target2],
-        arity: 2,
     }
 }
 
@@ -80,7 +103,6 @@ pub fn phase(target: usize) -> Operation {
             [Complex::new(0.0, 0.0), Complex::new(0.0, 1.0)]
         ],
         targets: vec![target],
-        arity: 1,
     }
 }
 
@@ -88,7 +110,6 @@ pub fn not(target: usize) -> Operation {
     Operation {
         matrix: real_to_complex(array![[0.0, 1.0], [1.0, 0.0]]),
         targets: vec![target],
-        arity: 1,
     }
 }
 
@@ -99,7 +120,6 @@ pub fn pauli_y(target: usize) -> Operation {
             [Complex::new(0.0, 1.0), Complex::new(0.0, 0.0)]
         ],
         targets: vec![target],
-        arity: 1,
     }
 }
 
@@ -107,7 +127,6 @@ pub fn pauli_z(target: usize) -> Operation {
     Operation {
         matrix: real_to_complex(array![[1.0, 0.0], [0.0, -1.0]]),
         targets: vec![target],
-        arity: 1,
     }
 }
 
@@ -120,18 +139,18 @@ pub fn toffoli(controls: &Vec<usize>, target: usize) -> Operation {
 
     // Creates an empty (2^n * 2^n) matrix and starts to fill it in as an identity matrix
     let mut matrix: Array2<f64> = Array2::<f64>::zeros((n, n));
-    for i in 0..n-2 { // Does not fill in the last two rows
+    for i in 0..n - 2 {
+        // Does not fill in the last two rows
         matrix.row_mut(i)[i] = 1.0;
     }
 
     // The last two rows are to be "swapped", finalizing the not part of the matrix
-    matrix.row_mut(n-1)[n-2] = 1.0;
-    matrix.row_mut(n-2)[n-1] = 1.0;
-    
-    Operation { 
+    matrix.row_mut(n - 1)[n - 2] = 1.0;
+    matrix.row_mut(n - 2)[n - 1] = 1.0;
+
+    Operation {
         matrix: real_to_complex(matrix),
-        targets: targets,
-        arity: controls.len()+1 
+        targets,
     }
 }
 
@@ -139,13 +158,12 @@ pub fn oracle_operation(regsize: usize, winner: usize) -> Operation {
     let n: usize = (2 as usize).pow(regsize as u32);
     let mut matrix: Array2<f64> = Array2::<f64>::zeros((n, n));
     for i in 0..n {
-        matrix.row_mut(i)[i] = if i == winner {-1.0} else {1.0};
+        matrix.row_mut(i)[i] = if i == winner { -1.0 } else { 1.0 };
     }
 
     Operation {
         matrix: real_to_complex(matrix),
         targets: vec![0],
-        arity: regsize
     }
 }
 
@@ -156,25 +174,20 @@ pub fn cz(controls: &Vec<usize>, target: usize) -> Operation {
     let n: usize = (2 as usize).pow(targets.len() as u32);
 
     let mut matrix: Array2<f64> = Array2::<f64>::zeros((n, n));
-    for i in 0..n-1 { 
+    for i in 0..n - 1 {
         matrix.row_mut(i)[i] = 1.0;
     }
-    matrix.row_mut(n-1)[n-1] = -1.0;
+    matrix.row_mut(n - 1)[n - 1] = -1.0;
 
-    Operation { 
+    Operation {
         matrix: real_to_complex(matrix),
         targets,
-        arity: controls.len()+1 
     }
-}
-
-fn real_to_complex(matrix: Array2<f64>) -> Array2<Complex<f64>> {
-    matrix.map(|e| e.into())
 }
 
 #[cfg(test)]
 mod tests {
-    use super::{OperationTrait, toffoli};
+    use super::{toffoli, OperationTrait};
     use ndarray::Array2;
     use num::Complex;
 
@@ -222,7 +235,11 @@ mod tests {
     #[test]
     fn toffoli2_equals_cnot() {
         let toffoli_generated_cnot = toffoli(&vec![0], 1);
-        assert!(matrix_is_equal(toffoli_generated_cnot.matrix(), cnot(0, 1).matrix(), 1e-8));
+        assert!(matrix_is_equal(
+            toffoli_generated_cnot.matrix(),
+            cnot(0, 1).matrix(),
+            1e-8
+        ));
     }
 
     fn matrix_is_equal(a: Array2<Complex<f64>>, b: Array2<Complex<f64>>, tolerance: f64) -> bool {

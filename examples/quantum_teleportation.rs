32fc1d58cb1fb6a0d29e2d764818204a9970aece
gustav <gustav.kalander@hotmail.se>
2023-04-13 11:04:53 +0200

    Improve test for quantum teleportation

--- examples/quantum_teleportation.rs
+++ examples/quantum_teleportation.rs
@@ -2,23 +2,22 @@ use clap::Parser;
 
 use colored::Colorize;
 use log::debug;
+use ndarray::{array, linalg, Array2, ArrayBase, Dim, OwnedRepr};
 use num::traits::Pow;
 use num::Zero;
+use quaru::math::{c64, equal_qubits, to_qbit_vector};
 use quaru::math::{limit_denominator, modpow};
 use quaru::{operation, register::Register};
 use rand::Rng;
 use stopwatch::Stopwatch;
-use ndarray::{Array2, ArrayBase, Dim, OwnedRepr, array, linalg};
-use quaru::math::{c64, equal_qubits, to_qbit_vector};
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Args {
     // We will teleport the qubit [a+bi, c+di].
-
     #[arg(short, long, default_value_t = 0.8)]
     a: f64,
- 
+
     #[arg(short, long, default_value_t = 0.0)]
     b: f64,
 
@@ -45,7 +44,10 @@ fn main() {
 
 fn test_quantum_teleportation(q0: Array2<c64>) -> (bool, Array2<c64>) {
     // State with three qubits: two zeroes and q0.
-    let new_state: Array2<c64> = linalg::kron(&linalg::kron(&to_qbit_vector(&false), &to_qbit_vector(&false)), &q0.clone());
+    let new_state: Array2<c64> = linalg::kron(
+        &linalg::kron(&to_qbit_vector(&false), &to_qbit_vector(&false)),
+        &q0.clone(),
+    );
 
     // Create register with state new_state
     let mut reg = Register::new(&[false; 3]);
@@ -102,11 +104,24 @@ pub fn get_state_of_qubit(
 #[cfg(test)]
 mod tests {
     use ndarray::array;
+    use num::{integer::Roots, traits::Pow};
     use quaru::math::c64;
+    use rand::Rng;
 
-    fn quantum_teleportation(){
-        let q0 = array![[c64::new(0.8, 0.0)], [c64::new(0.0, -0.6)]];
-        let (is_equal, _) = super::test_quantum_teleportation(q0);
-        assert!(is_equal);
+    #[test]
+    fn quantum_teleportation() {
+        let mut rng = rand::thread_rng();
+        for i in 0..20 {
+            // Random complex number with norm <= 1
+            let a = c64::from_polar(rng.gen_range(0.0 .. 1.0), rng.gen_range(0.0 .. 2.0 * std::f64::consts::PI));
+            // Random complex number such that norm(a)^2 + norm(b)^2 = 1
+            let b = c64::from_polar((1.0 - a.norm().powi(2)).sqrt(), rng.gen_range(0.0 .. 2.0 * std::f64::consts::PI));
+            
+            assert!(((a*a).norm() + (b*b).norm() - 1.0).abs() < 1e-10);
+
+            let q0 = array![[a], [b]];
+            let (is_equal, _) = super::test_quantum_teleportation(q0);
+            assert!(is_equal);
+        }
     }
 }

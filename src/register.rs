adae6a0f3bba311fe9878ac80c0aafd3cc450624
Elis Kleen <eliskleen00@gmail.com>
2023-05-10 17:09:18 +0200

    extracted main functionality to a function in order to see what needs to be parallelized

--- src/register.rs
+++ src/register.rs
@@ -4,9 +4,10 @@ use crate::{
     operation::{Operation, QuantumOperation},
 };
 use ndarray::{array, linalg, Array1, Array2};
-use num::traits::Pow;
 use rand::prelude::*;
-
+use std::sync::mpsc;
+use std::thread;
+use std::time::Duration;
 /// Errors which can occur when an operation is applied on the register.
 #[derive(Debug, PartialEq, Eq)]
 pub enum OperationError {
@@ -152,40 +153,52 @@ impl Register {
                 return Err(OperationError::InvalidTarget(target));
             }
         }
+        
         let mut new_state = self.state.clone();
+        let mut perm_and_res: Vec<(Vec<i32>, Array1<c64>)> = Vec::new();
+        //this loop should be possible to parallelize 
         for index in 0..1 << self.size() {
-            if op.targets().iter().all(|&x| (index >> x) & 1 == 0) {
-                //All targets bitpositions are 0
-                //Find all permutations where the target bits are changed but all other bits are the same
-                let mut permutations = Vec::new();
-                //Loop over the number of possible permutations of the target bits
-                for i in 0..1 << op.arity() {
-                    //set the base permutation to the current index
-                    let mut permutation = index;
-                    for (j, &target) in op.targets().iter().enumerate() {
-                        //For each target bit, we need to check if the jth bit of i is 1
-                        //if the jth bit of i is 1, then we need to flip the jth bit of the target
-                        if (i >> j) & 1 == 1 {
-                            permutation |= 1 << target;
-                        }
-                    }
-                    permutations.push(permutation);
-                }
-                //create the array containing the values of the affected qubits
-                let mut arr: Array1<c64> = Array1::zeros(1 << op.arity() as usize);
-                for (i, &p) in permutations.iter().enumerate() {
-                    arr[i] = self.state[[(p) as usize, 0]];
-                }
-                //apply the operation to the affected qubits
-                let res = op.matrix().dot(&arr);
-                //update the state
-                for (i, &p) in permutations.iter().enumerate() {
-                    new_state[[p as usize, 0]] = res[i];
-                }
+            perm_and_res.push(self.apply_gate_to_index(op, index));
+        }
+
+        for (permutations, res) in perm_and_res {
+            for (i, &p) in permutations.iter().enumerate() {
+                new_state[[p as usize, 0]] = res[i];
             }
         }
+
         self.state = new_state;
         Ok(self)
+    }
+
+    fn apply_gate_to_index(&self, op: &Operation, index: i32) -> (Vec<i32>, Array1<c64>) {
+        if op.targets().iter().all(|&x| (index >> x) & 1 == 0) {
+            //All targets bitpositions are 0
+            //Find all permutations where the target bits are changed but all other bits are the same
+            let mut permutations = Vec::new();
+            //Loop over the number of possible permutations of the target bits
+            for i in 0..1 << op.arity() {
+                //set the base permutation to the current index
+                let mut permutation = index;
+                for (j, &target) in op.targets().iter().enumerate() {
+                    //For each target bit, we need to check if the jth bit of i is 1
+                    //if the jth bit of i is 1, then we need to flip the jth bit of the target
+                    if (i >> j) & 1 == 1 {
+                        permutation |= 1 << target;
+                    }
+                }
+                permutations.push(permutation);
+            }
+            //create the array containing the values of the affected qubits
+            let mut arr: Array1<c64> = Array1::zeros(1 << op.arity() as usize);
+            for (i, &p) in permutations.iter().enumerate() {
+                arr[i] = self.state[[(p) as usize, 0]];
+            }
+            //apply the operation to the affected qubits
+            let res = op.matrix().dot(&arr);
+            return (permutations, res);
+        }
+        (vec![], Array1::zeros(0))
     }
 
     pub fn try_apply(&mut self, op: &Operation) -> Result<&mut Self, OperationError> {
